2ff2db6c8e09ea817dadedb4d29074acebe1181c
Alberto Zordan <zorberto@gmail.com>
2020-07-14 16:02:41 +0200

    To who is "spying" me: is it right? (done!)

--- js/script.js
+++ js/script.js
@@ -2,15 +2,37 @@
 // ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
 // Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 
-// Creo un ciclo FOR sull'indice 1<i<=100 che viene stampato con messaggi diversi a seconda che sia multiplo di 15 (cioè di 3 && anche di 5) o sia solo multiplo di 3 o solo di 5
+// for (var i = 1; i <= 100; i++) {
+//     if (i % 15 == 0) {
+//         console.log('FizzBuzz');
+//     }   else if (i % 3 == 0) {
+//         console.log('Fizz');
+//     }   else if (i % 5 == 0) {
+//         console.log('Buzz');
+//     }   else {
+//         console.log(i);
+//     }
+// }
+
+// Inizializzo la var stampa a stringa vuota
+var stampa = '';
+
+// Creo un ciclo FOR sull'indice 1<i<=100 che viene stampato con messaggi diversi a seconda che sia multiplo di 15 (cioè di 3 && anche di 5) o sia solo multiplo di 3 o solo di 5. Al verificarsi di ogni condizione aggiorno la var stampa concatenandone i valori
 for (var i = 1; i <= 100; i++) {
     if (i % 15 == 0) {
-        console.log('FizzBuzz');
+        stampa += ' FizzBuzz';
     }   else if (i % 3 == 0) {
-        console.log('Fizz');
+        stampa += ' Fizz';
     }   else if (i % 5 == 0) {
-        console.log('Buzz');
+        stampa += ' Buzz';
     }   else {
-        console.log(i);
+        stampa += ' ' + i;
+    }
+    // Mando a capo il testo visualizzato a ogni multiplo di 10
+    if (i % 10 == 0) {
+        stampa += '<br>';
     }
 }
+
+// Stampa a video della stringa concatenata
+document.getElementById('stampa').innerHTML = stampa;
